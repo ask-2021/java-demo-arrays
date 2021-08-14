699fe0cbc001b69c6742e6d61fc18e6cbad1cbd4
AsthaSingh <asthasingh@gmail.com>
2021-08-14 17:22:22 -0500

    Adding main method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,17 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Arrays class");
+
+        int[] array = new int[5];
+        array[0] = 2;
+        array[1] = 5;
+        array[3] = 10;
+        array[4] = 15;
+
+        for (int index = 0 ; index < array.length ; index++) {
+
+            System.out.println("The value in " +index +"th index is: " + array[index]);
+
+        }
     }
 }
